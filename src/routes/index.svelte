34ec30ed068645a0b16a770061d43b08478c1ef6
Mike Brandon <bkb.braock@gmail.com>
2021-10-12 10:55:39 +0300

    add: page title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@ import { onMount } from "svelte";
 import { ethers } from "ethers";
 import ScorpyNFT from "$lib/utils/scorpyNFT.json";
 import { mintedTotalStore } from "$lib/store/store";
+import { Jumper } from 'svelte-loading-spinners';
 
 	let currentAccount = "";
     const CONTRACT_ADDRESS = '0xa81224A420951797DE86309c9FB1C988b6C23025';
@@ -10,6 +11,7 @@ import { mintedTotalStore } from "$lib/store/store";
     mintedTotalStore.set(null);
     let update = 0;
     let mintedLink = null;
+    let loading = false;
 
 	async function checkIfWalletIsConnected() {
 		const { ethereum } = window;
@@ -129,9 +131,11 @@ import { mintedTotalStore } from "$lib/store/store";
 				let nftTxn = await connectedContract.makeNFT();
 
 				console.log(`Mining...Please Wait.`);
+                loading = true;
 				await nftTxn.wait();
 
 				console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+                loading = false;
 			} else {
 				console.log('Ethereum Object Doesn\'t Exist');
 			}
@@ -171,6 +175,10 @@ import { mintedTotalStore } from "$lib/store/store";
 	})
 </script>
 
+<svelte:head>
+	<title>Scorpy NFTs</title>
+</svelte:head>
+
 <main>
     <div class="App">
         <div class="container">
@@ -187,6 +195,12 @@ import { mintedTotalStore } from "$lib/store/store";
 				<button on:click={askContractToMintNft} class="cta-button connect-wallet-button" disabled={$mintedTotalStore>=30}>
 					Mint NFT
 				</button>
+            {/if}
+            {#if loading}
+                <div class = "spinner">
+                    <Jumper size="100" color="#35AEE2" unit="px" duration="1s"></Jumper>
+                    <p class='footer-text'>Please Wait...</p>
+                </div>
             {/if}
             {#if mintedLink}
                 <a href={mintedLink}>
@@ -206,6 +220,11 @@ import { mintedTotalStore } from "$lib/store/store";
                 </p>
             {/key}
           {/if}
+            <a href='https://testnets.opensea.io/collection/scorpynft-uvvpnx3gv6'>
+                <button class="cta-button connect-wallet-button">
+                    🌊 View Collection on OpenSea
+                </button>
+            </a>
           <div class="footer-container">
             <svg class='twitter-logo' viewBox="0 0 24 24" aria-hidden="true"><g fill=#1DA1F2><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
             <a
@@ -324,6 +343,14 @@ import { mintedTotalStore } from "$lib/store/store";
         color: white;
         font-size: 16px;
         font-weight: bold;
+    }
+
+    .spinner {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding-top: 1rem;
+        padding-bottom: 1rem;
     }
 
     /* KeyFrames */
